5722e39f75b76a1fce6afdc5fbf3a4f5259e0d10
= <student@appacademy.io>
2013-08-26 08:46:08 -0700

    More stuff.

--- snake.js
+++ snake.js
@@ -2,7 +2,7 @@ var Board = (function () {
 
 	function Board (dim) {
 		this.head = new Board.Segment(4, 4);
-		this.lastDirection = { x: 0, y: 0};
+		this.lastDirection = { x: -1, y: 0};
 		this.segments = [this.head];
 		this.apples = [];
 		this.apple(5, 5);
@@ -28,44 +28,33 @@ var Board = (function () {
 			var newRow = [];
 
 			for (var col = 0; col < this.dim; col++) {
-				var runner = this.head;
-				var occupied = false;
-
-				for (var i = 0; i < this.apples.length; i++) {
-					if (this.apples[i].x === col && this.apples[i].y === row) {
-						newRow.push("A");
-						occupied = true;
-					}
-				}
-				while (runner) {
-					if (runner.x === col && runner.y == row) {
-						newRow.push("*");
-						occupied = true;
-					}
-
-					runner = runner.child;
-				}
-
-				if (!occupied) {
 					newRow.push("_");
-				}
 			}
-
 			board.push(newRow);
 		}
-		console.log(board);
+
+		for (var i = 0; i < this.apples.length; i++) {
+			board[this.apples[i].y][this.apples[i].x] = "A";
+			}
+
+		var runner = this.head;
+		while (runner) {
+			board[runner.y][runner.x] = "*";
+			runner = runner.child;
+		}
+
+		return board;
 	}
 
 	Board.prototype.setDirection = function (x, y) {
 		this.direction = {x: x, y: y};
 	}
-
-	//dir is unit vector {x: x, y: y}.
 	Board.prototype.move = function () {
 		var dir = this.direction || this.lastDirection;
 
 		if (this.isInvalidDir(dir)) {
 			console.log("Invalid move!");
+			this.direction = this.lastDirection;
 			return false;
 		}
 
@@ -73,6 +62,8 @@ var Board = (function () {
 									y: this.head.y + dir.y};
 		if (this.isInvalidMove(newPos)) {
 			console.log("You're going off screen!");
+			this.setDirection(0, 0);
+			this.lastDirection = this.direction;
 			return false;
 		}
 
@@ -85,6 +76,8 @@ var Board = (function () {
 				if (apple.x === newPos.x && apple.y === newPos.y) {
 					this.apples.splice(i, 1);
 				}
+				this.apple(Math.floor(Math.random() * this.dim),
+									 Math.floor(Math.random() * this.dim));
 			}
 		}
 		this.slide(dir);
@@ -99,7 +92,7 @@ var Board = (function () {
 	}
 
 	Board.prototype.isInvalidMove = function (move) {
-		return (move.x > this.dim || move.y > this.dim || move.x < 0 || move.y < 0);
+		return (move.x >= this.dim || move.y >= this.dim || move.x < 0 || move.y < 0);
 	}
 
 	Board.prototype.hasApple = function (dir) {
@@ -118,37 +111,46 @@ var Board = (function () {
 	}
 
 	Board.prototype.slide = function (dir) {
-		var runner = this.head;
 
+		//Slides all back segments forward
+
+		var runner = this.head;
 		while (runner.child !== null) {
 			runner.child.x = runner.x;
 			runner.child.y = runner.y;
 			runner = runner.child;
 		}
+			this.head.x += dir.x;
+			this.head.y += dir.y;
 
-		this.head.x += dir.x;
-		this.head.y += dir.y;
 	}
 
 	Board.prototype.addSegment = function (x, y) {
 		var newHead = new Board.Segment(x, y);
 		newHead.child = this.head;
 		this.head = newHead;
+
+		var runner = this.head;
+		while(runner){
+			console.log(runner);
+			runner = runner.child;
+		}
+
 	}
 
 	return Board;
 })();
 
-var b = new Board(10);
-console.log();
-b.display();
-b.setDirection(1, 0);
-b.move();
-console.log();
-b.display();
-b.setDirection(0, 1);
-b.move();
-console.log();
-b.display();
-b.move();
-b.display();
+// var b = new Board(10);
+// console.log();
+// b.display();
+// b.setDirection(1, 0);
+// b.move();
+// console.log();
+// b.display();
+// b.setDirection(0, 1);
+// b.move();
+// console.log();
+// b.display();
+// b.move();
+// b.display();
--- snakeUI.js
+++ snakeUI.js
@@ -1,30 +1,55 @@
 var SnakeUI = (function (SnakeUI) {
 	SnakeUI.start = function () {
 		SnakeUI.bindKeys();
+		this.tmr = setInterval(SnakeUI.moveGame, 250);
 	};
 	SnakeUI.BoardSize = 10;
-	SnakeUI.Game = new Board(SnakeUI.BoardSize);
+	var Game = SnakeUI.Game = new Board(SnakeUI.BoardSize);
 	SnakeUI.bindKeys = function () {
 		$('html').keydown(function (event) {
 			switch (event.keyCode) {
 			case 37:
 				//Left
-				console.log("left");
+				// console.log("left");
+				Game.setDirection(-1, 0);
 				break;
 			case 38:
 				//up
-				console.log("up");
+				// console.log("up");
+				Game.setDirection(0, -1);
 				break;
 			case 39:
 				//right
-				console.log("right");
+				// console.log("right");
+				Game.setDirection(1,0);
 				break;
 			case 40:
 				//down
-				console.log("down");
+				// console.log("down");
+				Game.setDirection(0,1);
 				break;
 			}
 		});
+	};
+
+	SnakeUI.moveGame = function () {
+		if (Game.move())
+		{
+			$(".bodyPart").remove();
+			$('pre').text(Game.display().join("\n"));
+			var runner = Game.head;
+			while(runner) {
+				var bodyPart = $("<div class='bodyPart'></div>").css({
+					left: runner.x * 10,
+					top: runner.y * 10
+				});
+				$('body').append(bodyPart);
+				runner = runner.child;
+			}
+		}
+		else {
+			clearInterval(this.tmr);
+		}
 	};
 	return SnakeUI;
 })(SnakeUI || {});
